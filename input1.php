26589a8767f27da201199f92d6c3537733eba766
desandro <david@desandro.com>
2009-11-23 21:57:57 -0500

    remove commented out template

--- input1.php
+++ input1.php
@@ -15,19 +15,6 @@
     
     <form action="input2.php" method="post">
         
-        <!-- <p>
-                 <label for="book">Name: </label>
-                 <select name="book" id="book">
-                     <option>The Outsider</option>
-                     <option>The Rebel</option>
-                     <option>The Plague</option>
-                     <option>Animal Farm</option>
-                     <option>Nineteen Eighty-Four</option>
-                     <option>Down and Out in Paris and London</option>
-                 </select>
-             </p> -->
-        
-        
         <?php 
             radioInputs('Number of Races', 'race_count', array(1,3,4,8,10) ); 
             radioInputs('Number of Players', 'player_count', array(2,3,4) ); 
