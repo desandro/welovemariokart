c6c853f838a7e5726310a080ddb4ce996c88dafe
daveatnclud <dave@nclud.com>
2009-12-18 12:10:10 -0500

    Add html5 shiv for IE

--- _base/includes/templates/html_head.php
+++ _base/includes/templates/html_head.php
@@ -4,5 +4,7 @@
 	<meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
 
     <link rel="stylesheet" href="_base/css/master.css" type="text/css" media="screen" />
+
+    <!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
     
     <script type="text/javascript" src="_base/js/jquery-1.3.2.min.js" charset="utf-8"></script>
