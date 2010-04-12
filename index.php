682a79a9e1e2f8b9b840c07ab5abbb4f7c7d48c7
daveatnclud <dave@nclud.com>
2010-04-12 18:07:06 -0400

    Add index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+    include('_base/includes/config.php');
+    include('_base/includes/templates/html_head.php');
+?>
+
+
+    <title>welovemariokart</title>
+
+</head>
+<body>
+
+    
+    <div id="wrap">
+        
+        <?php include('_base/includes/templates/header.php'); ?>
+
+        <h1>welovemariokart</h1>
+
+        <p><a href="setup_round.php">Setup Round</a></p>
+
+        
+        <?php include('_base/includes/templates/footer.php'); ?>
+
+    </div> <!-- /#wrap -->
+
+    
+</body>
+</html>
