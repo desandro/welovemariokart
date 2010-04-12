2137e5fb0cab9dde59eb9eccf8bdab10b1b5210d
daveatnclud <dave@nclud.com>
2010-04-12 18:07:18 -0400

    Change font to Adrianna

--- _base/includes/templates/html_head.php
+++ _base/includes/templates/html_head.php
@@ -8,5 +8,5 @@
     <!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
     
     <script type="text/javascript" src="_base/js/jquery-1.4.min.js" charset="utf-8"></script>
-    <script type="text/javascript" src="http://use.typekit.com/mgj8otb.js"></script>
+    <script type="text/javascript" src="http://use.typekit.com/gmb0cdb.js"></script>
     <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
